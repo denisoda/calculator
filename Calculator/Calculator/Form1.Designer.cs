adb3f466e6a4aacf3ce5df5092ac97bf95edb9ca
Ilya Dzeraziak <ilya.by@bk.ru>
2017-06-13 23:08:02 +0300

    Added license file and general files

---
+++ Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.MainLB = new System.Windows.Forms.Label();
+            this.ZeroButton = new System.Windows.Forms.Button();
+            this.Plus = new System.Windows.Forms.Button();
+            this.SUMM = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(53, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(69, 122);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(49, 47);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(126, 122);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(45, 47);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(10, 175);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(52, 48);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(68, 175);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(50, 48);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(124, 175);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(47, 48);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(10, 229);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(53, 44);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(69, 229);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(49, 44);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(124, 229);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(47, 44);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 15);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(157, 47);
+            this.textBox1.TabIndex = 10;
+            // 
+            // MainLB
+            // 
+            this.MainLB.AutoSize = true;
+            this.MainLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MainLB.Location = new System.Drawing.Point(16, 33);
+            this.MainLB.Name = "MainLB";
+            this.MainLB.Size = new System.Drawing.Size(25, 25);
+            this.MainLB.TabIndex = 12;
+            this.MainLB.Text = "0";
+            this.MainLB.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ZeroButton
+            // 
+            this.ZeroButton.Location = new System.Drawing.Point(69, 279);
+            this.ZeroButton.Name = "ZeroButton";
+            this.ZeroButton.Size = new System.Drawing.Size(49, 42);
+            this.ZeroButton.TabIndex = 13;
+            this.ZeroButton.Text = "0";
+            this.ZeroButton.UseVisualStyleBackColor = true;
+            this.ZeroButton.Click += new System.EventHandler(this.ZeroButton_Click);
+            // 
+            // Plus
+            // 
+            this.Plus.Location = new System.Drawing.Point(10, 279);
+            this.Plus.Name = "Plus";
+            this.Plus.Size = new System.Drawing.Size(53, 42);
+            this.Plus.TabIndex = 14;
+            this.Plus.Text = "+";
+            this.Plus.UseVisualStyleBackColor = true;
+            this.Plus.Click += new System.EventHandler(this.Plus_Click);
+            // 
+            // SUMM
+            // 
+            this.SUMM.Location = new System.Drawing.Point(124, 279);
+            this.SUMM.Name = "SUMM";
+            this.SUMM.Size = new System.Drawing.Size(45, 42);
+            this.SUMM.TabIndex = 15;
+            this.SUMM.Text = "=";
+            this.SUMM.UseVisualStyleBackColor = true;
+            this.SUMM.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(111, -1);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(63, 13);
+            this.linkLabel1.TabIndex = 16;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "My vk page";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(126, 68);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(45, 48);
+            this.clearButton.TabIndex = 17;
+            this.clearButton.Text = "C";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button10.Location = new System.Drawing.Point(69, 69);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(49, 47);
+            this.button10.TabIndex = 18;
+            this.button10.Text = "*";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click_1);
+            // 
+            // minus
+            // 
+            this.minus.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.minus.Location = new System.Drawing.Point(13, 69);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(49, 47);
+            this.minus.TabIndex = 19;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(181, 330);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.SUMM);
+            this.Controls.Add(this.Plus);
+            this.Controls.Add(this.ZeroButton);
+            this.Controls.Add(this.MainLB);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Label LB;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label MainLB;
+        private System.Windows.Forms.Button ZeroButton;
+        private System.Windows.Forms.Button Plus;
+        private System.Windows.Forms.Button SUMM;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button minus;
+    }
+}
+
---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,656 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace WindowsFormsApp1
+{
+ 
+
+public partial class Form1 : Form
+    {
+        bool click;
+
+        private int zero = 0;
+        private double one = 1;
+        private double two = 2;
+        private double summ;
+        private int three = 3;
+        private int four = 4;
+        private int five= 5;
+        private int six = 6;
+        private int seven =7;
+        private int eight = 8;
+        private int nine = 9;
+        private float sum = 0;
+        private float buff = 0;
+        private int press_buff=0;
+        private bool count=false;
+        string sumString;
+
+
+
+        private int eq = 0;
+
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Buttons
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.press_buff++;
+            this.click = true;
+
+            
+            if (press_buff<=10||click==true)
+            {
+
+
+                if (press_buff <= 1 )
+                {
+                    MainLB.Text = "";
+                   
+
+
+                }
+                if (count)
+                {
+                    MainLB.Text = "";
+                    count = false;
+                }
+                if (press_buff<=10&&press_buff>=0);
+            {
+            MainLB.Text +="1";
+                
+            }
+                one = int.Parse(MainLB.Text);
+            
+            }
+
+            else
+            {
+                MessageBox.Show("You riched a num limit");
+            }
+            
+            
+                
+            
+
+            
+
+                
+
+                 
+               
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+            this.press_buff++;
+
+
+            if (press_buff <= 10 || click == true)
+            {
+
+
+                if (press_buff <= 1)
+                {
+                    MainLB.Text = "";
+
+
+                }
+                if (count)
+                {
+                    MainLB.Text = "";
+                    count = false;
+                }
+
+                if (press_buff <= 10 && press_buff >= 0) ;
+                {
+                    MainLB.Text += "2";
+
+                }
+                two = int.Parse(MainLB.Text);
+
+            }
+
+            else
+            {
+                MessageBox.Show("You riched a num limit");
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            this.press_buff++;
+
+
+            if (press_buff <= 10 || click == true)
+            {
+
+
+                if (press_buff <= 1)
+                {
+                    MainLB.Text = "";
+
+
+                }
+                if (count)
+                {
+                    MainLB.Text = "";
+                    count = false;
+                }
+
+                if (press_buff <= 10 && press_buff >= 0) ;
+                {
+                    MainLB.Text += "3";
+
+                }
+                three = int.Parse(MainLB.Text);
+
+            }
+
+            else
+            {
+                MessageBox.Show("You riched a num limit");
+            }
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            this.press_buff++;
+
+
+            if (press_buff <= 10 || click == true)
+            {
+
+
+                if (press_buff <= 1)
+                {
+                    MainLB.Text = "";
+
+
+                }
+
+                if (count)
+                {
+                    MainLB.Text = "";
+                    count = false;
+                }
+                if (press_buff <= 10 && press_buff >= 0) ;
+                {
+                    MainLB.Text += "4";
+
+                }
+                four = int.Parse(MainLB.Text);
+
+            }
+
+            else
+            {
+                MessageBox.Show("You riched a num limit");
+            }
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            this.press_buff++;
+
+
+            if (press_buff <= 10 || click == true)
+            {
+
+
+                if (press_buff <= 1)
+                {
+                    MainLB.Text = "";
+
+
+                }
+                if (count)
+                {
+                    MainLB.Text = "";
+                    count = false;
+                }
+
+                if (press_buff <= 10 && press_buff >= 0) ;
+                {
+                    MainLB.Text += "5";
+
+                }
+                five = int.Parse(MainLB.Text);
+
+            }
+
+            else
+            {
+                MessageBox.Show("You riched a num limit");
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            this.press_buff++;
+
+
+            if (press_buff <= 10 || click == true)
+            {
+
+
+                if (press_buff <= 1)
+                {
+                    MainLB.Text = "";
+
+
+                }
+                if (count)
+                {
+                    MainLB.Text = "";
+                    count = false;
+                }
+
+                if (press_buff <= 10 && press_buff >= 0) ;
+                {
+                    MainLB.Text += "6";
+
+                }
+                six = int.Parse(MainLB.Text);
+
+            }
+
+            else
+            {
+                MessageBox.Show("You riched a num limit");
+            }
+
+
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+            this.press_buff++;
+
+
+            if (press_buff <= 10 || click == true)
+            {
+
+
+                if (press_buff <= 1)
+                {
+                    MainLB.Text = "";
+
+
+                }
+                if (count)
+                {
+                    MainLB.Text = "";
+                    count = false;
+                }
+
+                if (press_buff <= 10 && press_buff >= 0) ;
+                {
+                    MainLB.Text += "7";
+
+                }
+                seven = int.Parse(MainLB.Text);
+
+            }
+
+            else
+            {
+                MessageBox.Show("You riched a num limit");
+            }
+
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+            this.press_buff++;
+
+
+            if (press_buff <= 10 || click == true)
+            {
+
+
+                if (press_buff <= 1)
+                {
+                    MainLB.Text = "";
+
+
+                }
+                if (count)
+                {
+                    MainLB.Text = "";
+                    count = false;
+                }
+                if (press_buff <= 10 && press_buff >= 0) ;
+                {
+                    MainLB.Text += "8";
+
+                }
+                eight = int.Parse(MainLB.Text);
+
+            }
+
+            else
+            {
+                MessageBox.Show("You riched a num limit");
+            }
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+            this.press_buff++;
+
+
+            if (press_buff <= 10 || click == true)
+            {
+
+
+                if (press_buff <= 1)
+                {
+                    MainLB.Text = "";
+
+
+                }
+                if (count)
+                {
+                    MainLB.Text = "";
+                    count = false;
+                }
+                if (press_buff <= 10 && press_buff >= 0) ;
+                {
+                    MainLB.Text += "9";
+
+                }
+                nine = int.Parse(MainLB.Text);
+
+            }
+
+            else
+            {
+                MessageBox.Show("You riched a num limit");
+            }
+
+        }
+
+        private void ZeroButton_Click(object sender, EventArgs e)
+        {
+            this.press_buff++;
+
+
+            if (press_buff <= 10 || click == true)
+            {
+
+
+                if (press_buff <= 1)
+                {
+                    MainLB.Text = "";
+
+
+                }
+                if (count)
+                {
+                    MainLB.Text = "";
+                    count = false;
+                }
+
+                if (press_buff <= 10 && press_buff >= 0) ;
+                {
+                    MainLB.Text += "0";
+
+                }
+                zero = int.Parse(MainLB.Text);
+
+            }
+
+            else
+            {
+                MessageBox.Show("You riched a num limit");
+            }
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            MainLB.Text = sumString;
+        }
+        #endregion
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            ProcessStartInfo pagelink = new ProcessStartInfo("https://vk.com/denisoda");
+            Process.Start(pagelink);
+        }
+
+        private void Plus_Click(object sender, EventArgs e)
+        {
+          click=true;
+
+
+            
+
+            if (MainLB.Text != "")
+            {
+                
+            switch (eq)
+            {
+
+                  
+
+                case 0:
+                
+                    one = double.Parse(MainLB.Text);
+                    summ = one;
+
+                    eq++;
+                    
+                    MainLB.Text = "";
+
+                    break;
+
+                case 1:
+                    two = double.Parse(MainLB.Text);
+                    summ += two;
+                  
+                    sumString = Convert.ToString(summ);
+
+                    MainLB.Text = "";
+                    count=true;
+                    MainLB.Text = sumString;
+                  
+                    eq++;
+                    break;
+
+                case 2:
+                    MainLB.Text = "";
+
+
+                    eq = 0;
+                    break;
+                        
+
+
+            }
+            }
+            else
+            {
+                MainLB.Text = "";
+            }
+
+
+
+
+            
+
+
+
+
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            MainLB.Text = "";
+            summ = 0;
+            eq=0;
+
+
+
+
+
+        }
+
+        private void button10_Click_1(object sender, EventArgs e)
+        {
+            click = true;
+
+
+
+
+            if (MainLB.Text != "")
+            {
+
+                switch (eq)
+                {
+
+
+
+                    case 0:
+
+                        one = double.Parse(MainLB.Text);
+                        summ = one;
+
+                        eq++;
+
+                        MainLB.Text = "";
+
+                        break;
+
+                    case 1:
+                        two = double.Parse(MainLB.Text);
+                        summ = two*summ;
+
+                        sumString = Convert.ToString(summ);
+
+                        MainLB.Text = "";
+                        count = true;
+                        MainLB.Text = sumString;
+
+                        eq++;
+                        break;
+
+                    case 2:
+                        MainLB.Text = "";
+
+
+                        eq = 0;
+                        break;
+
+
+
+                }
+            }
+            else
+            {
+                MainLB.Text = "";
+            }
+
+
+
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            click = true;
+
+
+
+
+            if (MainLB.Text != "")
+            {
+
+                switch (eq)
+                {
+
+
+
+                    case 0:
+
+                        one = double.Parse(MainLB.Text);
+                        summ = one;
+
+                        eq++;
+
+                        MainLB.Text = "";
+
+                        break;
+
+                    case 1:
+                        two = double.Parse(MainLB.Text);
+                        summ -= two;
+
+                        sumString = Convert.ToString(summ);
+
+                        MainLB.Text = "";
+                        count = true;
+                        MainLB.Text = sumString;
+
+                        eq++;
+                        break;
+
+                    case 2:
+                        MainLB.Text = "";
+
+
+                        eq = 0;
+                        break;
+
+
+
+                }
+            }
+            else
+            {
+                MainLB.Text = "";
+            }
+
+
+        }
+    }
+}
